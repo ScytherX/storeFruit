f0cc7b85f6eee66f3479a59a25bad482a0b3a926
ScytherX <inagatari@gmail.com>
2021-10-20 10:21:18 -0600

    Act 1

---
+++ storeFruit/screens/StoreScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text } from 'react-native';
+import { View, Text, Touchable, TouchableOpacity } from 'react-native'
+
+const store = () => {
+ return(
+  <View>  
+      <Button title="Administrador" style={styles.Button}
+        onPress={}
+      />
+      <Button title="Usuario" style={styles.Button}
+        onPress={}
+      />
+  </View>  
+ );
+}
+
+export default store;
